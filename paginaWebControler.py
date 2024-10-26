359644d78b2c1fc6b9826c8ae8d2c7da3604f4fd
moissoto <moissoto@gmail.com>
2024-10-26 01:37:45 -0600

    'Se importa el módulo de codificador'

--- paginaWebControler.py
+++ paginaWebControler.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request
 from calculadora import operacion
+from codificador import encode64,decode64
+
 
 app = Flask(__name__,template_folder='templates')
 
